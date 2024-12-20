58b0436d0b99c98d51cce6fcd9b116d39ec4ecbf
OneBeatTrue <knd-13@yandex.ru>
2024-12-20 18:41:12 +0300

    feat: add lab 5

--- js/feedback.js
+++ js/feedback.js
@@ -1,10 +1,10 @@
-const feedbackContainer = document.getElementById('feedbackContainer');
+const feedbackContainer = document.querySelector('.feedback-container');
 function loadFeedbacks() {
     const savedFeedbacks = JSON.parse(localStorage.getItem("feedbacks")) || [];
     savedFeedbacks.forEach(feedbackContent => {
         const feedback = document.createElement("div");
         feedback.classList.add("feedback-container__feedback");
-        feedback.innerHTML = `<b>${feedbackContent.name}</b><i>${feedbackContent.content}</i>`;
+        feedback.innerHTML = `<b>${feedbackContent.name}: </b><i>${feedbackContent.feedback}</i>`;
         feedbackContainer.appendChild(feedback);
     });
 }
@@ -12,21 +12,20 @@ function loadFeedbacks() {
 function addFeedback(name, feedback) {
     const feedback_elem = document.createElement("div");
     feedback_elem.classList.add("feedback-container__feedback");
-    feedback_elem.innerHTML = `<strong>${name}</strong><p>${feedback}</p>`;
+    feedback_elem.innerHTML = `<b>${name}: </b><i>${feedback}</i>`;
     feedbackContainer.appendChild(feedback_elem);
     const savedFeedbacks = JSON.parse(localStorage.getItem("feedbacks")) || [];
     savedFeedbacks.push({ name, feedback });
     localStorage.setItem("feedbacks", JSON.stringify(savedFeedbacks));
 }
 
-const form = document.getElementById("feedbackForm");
+const form = document.querySelector(".feedback-form");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
-    const name = document.querySelector(".name").value;
-    const feedback = document.querySelector(".feedback").value;
+    const name = document.getElementById("name").value;
+    const feedback = document.getElementById("feedback").value;
     addFeedback(name, feedback);
-    document.querySelector(".name").value = "";
-    document.querySelector(".feedback").value = "";
+    form.reset()
 });
 
 document.addEventListener("DOMContentLoaded", loadFeedbacks);
