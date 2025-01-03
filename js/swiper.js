f9b796d4c9cdb81f94de70ec01e929464c5c6658
OneBeatTrue <knd-13@yandex.ru>
2025-01-04 01:25:55 +0400

    fix: link to readme

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper-container', {
         loop: true,
-        spaceBetween: 1000,
+        spaceBetween: 10000,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
